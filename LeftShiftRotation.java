9b3792ea7f8b483c5fadff1191555b5ea6d158af
Ajith637464 <ajithuk112233@gmail.com>
2024-09-02 22:20:11 +0530

    Add files via upload

---
+++ LeftShiftRotation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class LeftShiftRotation
+{
+	static Scanner sc = new Scanner (System.in);
+	public static int[] leftShiftRotation(int []a,int n)
+	{
+		for (int i=1;i<=n;i++)
+		{
+			int first=a[a.length-1];
+			for (int j=a.length-1;j>0;j--)
+			{
+				a[j]=a[j-1];
+			}
+			a[0]=first;
+		}
+		return a;
+	}
+	public static void main(String[] args) 
+	{
+		int[]a={10,9,1,0,2,3,4,6,6};
+		System.out.println("\nGiven: "+Arrays.toString(a));
+		System.out.print("Enter the n value: ");
+		int n=sc.nextInt();
+
+		int[]res=leftShiftRotation(a,n);
+		System.out.println("\nOutput: "+Arrays.toString(res));
+	}
+}
+/*
+Given: [10, 9, 1, 0, 2, 3, 4, 6, 6]
+
+Output: [1, 2, 7, 8, 6, 5, 4, 3, 3]
+
+Product is: 25
+*/
